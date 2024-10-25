845e3fb990a5246aa7a46be7a712800170a6e51e
matthieu2022 <99349894+matthieu2022@users.noreply.github.com>
2024-10-25 15:32:55 +0200

    Update card.tsx
    Pour netlify

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,45 +1,39 @@
-import * as React from "react";
+import React from "react";
+import { cn } from "@/lib/utils";
 
-const Card = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  return (
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={`rounded-lg border bg-white shadow-sm ${className || ''}`}
+      className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
       {...props}
     />
-  );
-});
+  )
+);
 Card.displayName = "Card";
 
-const CardHeader = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  return (
+interface CardHeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const CardHeader = React.forwardRef<HTMLDivElement, CardHeaderProps>(
+  ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={`flex flex-col p-6 ${className || ''}`}
+      className={cn("flex flex-col space-y-1.5 p-6", className)}
       {...props}
     />
-  );
-});
+  )
+);
 CardHeader.displayName = "CardHeader";
 
-const CardContent = React.forwardRef
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      ref={ref}
-      className={`p-6 pt-0 ${className || ''}`}
-      {...props}
-    />
-  );
-});
+interface CardContentProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const CardContent = React.forwardRef<HTMLDivElement, CardContentProps>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  )
+);
 CardContent.displayName = "CardContent";
 
-export { Card, CardHeader, CardContent };
+export default Card;
