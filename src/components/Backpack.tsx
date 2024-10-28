87cc08ff142c8d00c90be6b9af057ebfb317d38f
matthieu2022 <99349894+matthieu2022@users.noreply.github.com>
2024-10-28 09:53:12 +0100

    Update Backpack.tsx

--- src/components/Backpack.tsx
+++ src/components/Backpack.tsx
@@ -23,7 +23,7 @@ export const Backpack = ({
               onDragOver={onDragOver}
               onDrop={(e) => onDrop(e, "backpack", index)}
               onTouchEnd={(e) => onTouchEnd?.(e, "backpack", index)}
-              className="h-24 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center ml-[30px]"
+              className="h-12 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center ml-3"
             >
               {skill && (
                 <div
@@ -53,7 +53,7 @@ export const Backpack = ({
               onDragOver={onDragOver}
               onDrop={(e) => onDrop(e, "backpack", index + 5)}
               onTouchEnd={(e) => onTouchEnd?.(e, "backpack", index + 5)}
-              className="h-24 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center ml-[35px]"
+              className="h-12 border-2 border-dashed border-gray-300 rounded-lg flex items-center justify-center ml-3"
             >
               {skill && (
                 <div
